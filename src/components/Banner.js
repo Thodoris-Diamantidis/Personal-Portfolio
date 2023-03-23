f2ec1a0bd5c8bb161bde84034997004350dd3f09
Thodoris-Diamantidis <thodoris_diamantidis@hotmail.com>
2023-03-23 14:05:33 +0200

    modified banner and added css for banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ import headerImg from '../assets/images/header-img.svg'
 export function Banner() {
   const [loopNum, setLoopNum] = useState(0)
   const [isDeleting, setIsDeleting] = useState(false)
-  const toRotate = ["Web Developer", "Web Designer"]
+  const toRotate = ["Web Developer", "Web Designer", "Python/Java enthusiast"]
   const [text, setText] = useState('')
   const [delta, setDelta] = useState(300- Math.random() * 100)
   const period = 1500
@@ -46,8 +46,11 @@ export function Banner() {
             <Row className='align-items-center'>
                 <Col xs={12} md={6} xl={7}>
                     <span className='tagline'>Welcome to my Portfolio</span>
-                    <h1>Hi I'm Theodore<span className='wrap'>{text}</span></h1>
-                    <P>Put something here later!</P>
+                    <h1>Hi I'm Theodore <span className='wrap'>{text}</span></h1>
+                    <p>In publishing and graphic design, Lorem ipsum is a placeholder text
+                         commonly used to demonstrate the visual form of a document or a 
+                         typeface without relying on meaningful content. Lorem ipsum may be 
+                         used as a placeholder before final copy is available</p>
                     <button onClick={ () => console.log('connent')}>Let's connect <ArrowRightSquare size={25}/></button>
                 </Col>
                 <Col xs={12} md={6} xl={5}>
