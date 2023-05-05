7188e27824ea2c951035b5dde1116fc078ac9cdb
Thodoris-Diamantidis <thodoris_diamantidis@hotmail.com>
2023-05-05 17:37:06 +0300

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,7 +22,7 @@ export const Projects = () => {
 
         {
             title: "Currently working on",
-            description: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available",
+            description: "There is more projects on the way.",
             imgUrl: chatBotImage,
             url: "EMPTY"
         }
